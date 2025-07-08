b14a038324f2c9e0ef13762c9b234af785722127
Vincentvirtuoso <vincentvirtuoso66@gmail.com>
2025-07-08 17:56:28 +0100

    latest monday 7/7/2025

--- src/pages/Student/StudentSchedules/StudentSchedules.jsx
+++ src/pages/Student/StudentSchedules/StudentSchedules.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
+import { useAuth } from '../../../context/AuthContext';
 import Schedule from '../../ClassRep/ClassSchedule/Schedule/Schedule';
 import { scheduleJson } from '../../ClassRep/ClassSchedule/assets';
 
 const StudentSchedules = () => {
+  const { user, setNavTitle } = useAuth();
+
+  useEffect(() => {
+    setNavTitle('My Class Schedules');
+  }, [setNavTitle]);
   return (
     <>
       <Schedule
